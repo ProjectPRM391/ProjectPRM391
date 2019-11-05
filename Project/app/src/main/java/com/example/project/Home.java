471364adea069bf532b8cc0cc167c0b14061ea98
kimpt142 <57012862+kimpt142@users.noreply.github.com>
2019-11-05 17:26:08 +0700

    kim
    commit search in homepage

--- Project/app/src/main/java/com/example/project/Home.java
+++ Project/app/src/main/java/com/example/project/Home.java
@@ -6,7 +6,11 @@ import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
@@ -56,4 +60,48 @@ public class Home extends AppCompatActivity {
 
         return list;
     }*/
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+       MenuInflater inflater = getMenuInflater();
+       inflater.inflate(R.menu.search_menu, menu);
+
+       MenuItem item = menu.findItem(R.id.search);
+       SearchView searchView = (SearchView) item.getActionView();
+
+       searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+           @Override
+           public boolean onQueryTextSubmit(String s) {
+               getSearch(s);
+               return false;
+           }
+
+           @Override
+           public boolean onQueryTextChange(String s) {
+               return false;
+           }
+       });
+       return super.onCreateOptionsMenu(menu);
+   }
+
+   public void getSearch(String s){
+       ArrayList<Story> searchList = new ArrayList<Story>();
+       DatabaseHelper db = new DatabaseHelper(getApplicationContext());
+       // get image from drawable
+       Bitmap images = BitmapFactory.decodeResource(getResources(),
+               R.drawable.image);
+
+        // convert bitmap to byte
+       ByteArrayOutputStream stream = new ByteArrayOutputStream();
+       images.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+       byte imageInByte[] = stream.toByteArray();
+
+       List<Story> contacts = new ArrayList<Story>();
+       contacts = db.getStoryByName(contacts, s);
+       for (Story cn : contacts) {
+           searchList.add(cn);
+       }
+       adapter = new CustomerListAdapter(this, searchList);
+       ListView dataList = (ListView) findViewById(R.id.listView);
+       dataList.setAdapter(adapter);
+   }
 }
